188b149cd86bd1e3b436c0a3d1abbeebfe32ebbd
KBerry4492 <kmb23@comcast.net>
2017-10-20 13:43:34 -0400

    Time to go to work soon!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,13 +21,100 @@ connection.connect(function(err) {
 
 function inStock() {
   console.log("Selecting all products...\n");
-  connection.query("SELECT id, item, price FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     // Log all results of the SELECT statement
 
     for (var i = 0; i < res.length; i++) {
-      console.log("Product ID: "+res[i].id+", Product: "+res[i].item+", Price: "+res[i].price+"\n");
+      console.log("Product ID: "+res[i].id+", Product: "+res[i].item+", Price: "+res[i].price+", Stock: "+res[i].stock+"\n");
     }
-    connection.end();
+    start();
   });
+}
+
+function start() {
+  inquirer
+    .prompt({
+      name: "option",
+      type: "list",
+      message: "Would you like to [SHOP] or [EXIT] the store?",
+      choices: ["SHOP", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.option.toUpperCase() === "SHOP") {
+        shOptions();
+      }
+      else {
+        endProgram();
+      }
+    });
+}
+
+function shOptions() {
+  // query the database for all items in store
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    var choiceArray = [];
+            for (var i = 0; i < res.length; i++) {
+              choiceArray.push(String(res[i].id));
+            }
+    // prompt user for what they want to buy
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          choices: choiceArray,
+          message: "What would you like to purchase?"
+        },
+        {
+          name: "num",
+          type: "input",
+          message: "How much would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem = {};
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].id === parseInt(answer.choice)) {
+            chosenItem = res[i];
+          }
+        }
+        var totalPrice = (chosenItem.price * answer.num);
+        var stockOut = chosenItem.stock - parseInt(answer.num);
+        // if there are stock remaining,
+        if (chosenItem.stock > 0 && stockOut >= 0) {
+          // sufficient stock, update DB to reflect purchase
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock: stockOut
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Item bought successfully!\n");
+              console.log("Total cost: $"+totalPrice+"\n");
+              console.log(stockOut+" "+chosenItem.item+" remaining.");
+              start();
+            }
+          );
+        }
+        else {
+          console.log("Not enough product, try again.");
+          start();
+        }
+      });
+  });
+}
+
+
+function endProgram(){
+  connection.end();
 }
